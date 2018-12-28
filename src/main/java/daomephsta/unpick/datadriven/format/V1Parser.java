22fc9a2d19fdaf09aa428a40ea3ee5b0d00c3b98
therealfarfetchd <git@marco-rebhan.de>
2018-12-28 21:35:49 +0100

    improve parser (comments everywhere, allow arbitrary spacing)

--- src/main/java/daomephsta/unpick/datadriven/format/V1Parser.java
+++ src/main/java/daomephsta/unpick/datadriven/format/V1Parser.java
@@ -11,49 +11,67 @@ import daomephsta.unpick.datadriven.*;
 public enum V1Parser
 {
 	INSTANCE;
-	
+
 	public void parse(LineNumberReader reader, Map<String, ConstantGroup> constantGroups, Map<String, TargetMethod> targetMethods) throws IOException
 	{
-		String line = "";
-		while((line = reader.readLine()) != null)
+		String line;
+		while ((line = reader.readLine()) != null)
 		{
-			//Ignore comments and blank lines
-			if (line.startsWith(";") || line.isEmpty())
-				continue;
-			else
+			line = stripComment(line).trim();
+			if (line.isEmpty()) continue;
+
+			String[] tokens = tokenize(line);
+			if (tokens.length == 0) continue;
+
+			switch (tokens[0])
 			{
-				String[] tokens = line.split("\\s");
-				if ("constant".equals(tokens[0]))
-				{
+				case "constant":
 					if (tokens.length != 4 && tokens.length != 6)
 						throw new UnpickSyntaxException(reader.getLineNumber(), "Unexpected token count. Expected 4 or 6. Found " + tokens.length);
-					
+
 					String group = tokens[1];
 					ConstantDefinition parsedConstant = parseConstantDefinition(tokens, reader.getLineNumber());
 					ConstantGroup constantGroup = constantGroups.computeIfAbsent(group, k -> new ConstantGroup());
 					constantGroup.add(parsedConstant);
-				}
-				else if("unpick".equals(tokens[0]))
-				{
+					break;
+				case "unpick":
 					TargetMethod parsedTargetMethod = parseTargetMethodDefinition(tokens, reader.getLineNumber());
 					targetMethods.put(parsedTargetMethod.getOwner() + '.' + parsedTargetMethod.getName() + parsedTargetMethod.getMethodDescriptor().getDescriptor(), parsedTargetMethod);
-				}
-				else
+					break;
+				default:
 					throw new UnpickSyntaxException(reader.getLineNumber(), "Unknown start token " + tokens[0]);
 			}
 		}
 	}
 
+	private String stripComment(String in)
+	{
+		int c = in.indexOf(';');
+		return c == -1 ? in : in.substring(0, c);
+	}
+
+	private String[] tokenize(String in)
+	{
+		List<String> result = new ArrayList<>();
+
+		for (String s : in.split("\\s"))
+		{
+			if (!s.isEmpty()) result.add(s);
+		}
+
+		return result.toArray(new String[0]);
+	}
+
 	private ConstantDefinition parseConstantDefinition(String[] tokens, int lineNumber)
-	{ 
+	{
 		String owner = tokens[2];
 		String name = tokens[3];
-		
+
 		if (tokens.length > 4)
 		{
-			try 
+			try
 			{
-				Type descriptor = Type.getType(tokens[5]); 
+				Type descriptor = Type.getType(tokens[5]);
 				String value = tokens[4];
 				return new ConstantDefinition(owner, name, descriptor, value);
 			}
@@ -62,7 +80,7 @@ public enum V1Parser
 				throw new UnpickSyntaxException(lineNumber, "Unable to parse descriptor " + tokens[4]);
 			}
 		}
-		
+
 		return new ConstantDefinition(owner, name);
 	}
 
@@ -70,11 +88,11 @@ public enum V1Parser
 	{
 		if (tokens.length < 4 || tokens.length % 2 != 0)
 			throw new UnpickSyntaxException(lineNumber, "Unexpected token count. Expected an even number greater than or equal to 4. Found " + tokens.length);
-		
+
 		String owner = tokens[1];
 		String name = tokens[2];
-		
-		try 
+
+		try
 		{
 			Type methodType = Type.getMethodType(tokens[3]);
 			Map<Integer, String> parameterConstantGroups = new HashMap<>();
@@ -88,7 +106,7 @@ public enum V1Parser
 					else
 						parameterConstantGroups.put(parameterIndex, tokens[p]);
 				}
-				catch(NumberFormatException e)
+				catch (NumberFormatException e)
 				{
 					throw new UnpickSyntaxException(lineNumber, "Could not parse " + tokens[p - 1] + " as integer", e);
 				}
