060218fd4a9a8ca1efab4697352e2bee22464990
Daomephsta <dtnilfisk@gmail.com>
2019-07-08 15:05:13 +1200

    Implement unit test for flag constants

--- src/test/java/daomephsta/unpick/tests/FlagUninliningTest.java
+++ src/test/java/daomephsta/unpick/tests/FlagUninliningTest.java
@@ -1,21 +1,121 @@
 package daomephsta.unpick.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.objectweb.asm.Opcodes.IOR;
+import static org.objectweb.asm.Opcodes.LOR;
+
+import org.junit.jupiter.api.Test;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.MethodNode;
+
+import daomephsta.unpick.AbstractInsnNodes;
+import daomephsta.unpick.constantmappers.IConstantMapper;
+import daomephsta.unpick.constantresolvers.ClasspathConstantResolver;
+import daomephsta.unpick.tests.lib.*;
+import daomephsta.unpick.transformers.ConstantUninliner;
+
 public class FlagUninliningTest
 {
-	public void test()
-	{
-		intTest(0);
-		intTest(0b1);
-		intTest(0b11);
-		intTest(0b1011);
+	@SuppressWarnings("unused")
+	private static class Constants
+	{	
+		public static final int INT_FLAG_BIT_0 = 1 << 0,
+								INT_FLAG_BIT_1 = 1 << 1,
+								INT_FLAG_BIT_2 = 1 << 2,
+								INT_FLAG_BIT_3 = 1 << 3;
 		
-		longTest(0);
-		longTest(0b1);
-		longTest(0b11);
-		longTest(0b1011);
+		public static final long LONG_FLAG_BIT_0 = 1 << 0,
+								 LONG_FLAG_BIT_1 = 1 << 1,
+								 LONG_FLAG_BIT_2 = 1 << 2,
+								 LONG_FLAG_BIT_3 = 1 << 3;
 	}
 	
-	private static void intTest(int test) {}
+	@SuppressWarnings("unused")
+	private static class Methods
+	{
+		private static void intConsumer(int test) {}
 
-	private static void longTest(long test) {}
+		private static void longConsumer(long test) {}
+	}
+	
+	@Test
+	public void testIntFlags()
+	{
+		Integer[] testConstants = {0b1100, 0b0100, 0b1010, 0b0111};
+		String[][] expectedConstantCombinations = 
+			{
+				new String[] {"INT_FLAG_BIT_2", "INT_FLAG_BIT_3"},
+				new String[] {"INT_FLAG_BIT_2"},
+				new String[] {"INT_FLAG_BIT_1", "INT_FLAG_BIT_3"},
+				new String[] {"INT_FLAG_BIT_0", "INT_FLAG_BIT_1", "INT_FLAG_BIT_2"},
+			};
+		String[] constantNames = {"INT_FLAG_BIT_0", "INT_FLAG_BIT_1", "INT_FLAG_BIT_2", "INT_FLAG_BIT_3"};
+		testConstants(testConstants, expectedConstantCombinations, constantNames, "intConsumer", "(I)V");
+	}
+
+	@Test
+	public void testLongFlags()
+	{
+		Long[] testConstants = {0b1100L, 0b0100L, 0b1010L, 0b0111L};
+		String[][] expectedConstantCombinations = 
+			{
+				new String[] {"LONG_FLAG_BIT_2", "LONG_FLAG_BIT_3"},
+				new String[] {"LONG_FLAG_BIT_2"},
+				new String[] {"LONG_FLAG_BIT_1", "LONG_FLAG_BIT_3"},
+				new String[] {"LONG_FLAG_BIT_0", "LONG_FLAG_BIT_1", "LONG_FLAG_BIT_2"},
+			};
+		String[] constantNames = {"LONG_FLAG_BIT_0", "LONG_FLAG_BIT_1", "LONG_FLAG_BIT_2", "LONG_FLAG_BIT_3"};
+		testConstants(testConstants, expectedConstantCombinations, constantNames, "longConsumer", "(J)V");
+	}
+
+	private void testConstants(Object[] testConstants, String[][] expectedConstantCombinations, String[] constantNames, String constantConsumerName, String constantConsumerDescriptor)
+	{
+		IConstantMapper mapper = MockConstantMapper.builder(new ClasspathConstantResolver())
+				.flagConstantGroup("test")
+					.defineAll(Constants.class, constantNames)
+				.add()
+				.targetMethod(Methods.class, constantConsumerName, constantConsumerDescriptor)
+					.remapParameter(0, "test")
+				.add()
+				.build();
+		
+		Class<?> unboxedType = TestUtils.unboxedType(testConstants.getClass().getComponentType());
+		String constantTypeDescriptor = Type.getDescriptor(unboxedType);
+		int orOpcode;
+		if (constantTypeDescriptor.equals(Type.LONG_TYPE.getDescriptor()))
+			orOpcode = LOR;
+		else if(constantTypeDescriptor.equals(Type.INT_TYPE.getDescriptor()))
+			orOpcode = IOR;
+		else
+			throw new IllegalArgumentException("Expected long or int test constants but got " + unboxedType.getName() + " test constants");
+		ConstantUninliner uninliner = new ConstantUninliner(mapper);
+		
+		for (int i = 0; i < testConstants.length; i++)
+		{
+			Object expectedLiteralValue = testConstants[i];
+			String[] expectedConstantCombination = expectedConstantCombinations[i];
+			MethodNode methodInvocation = InstructionMocker.mockInvokeStatic(Methods.class, 
+					constantConsumerName, constantConsumerDescriptor, testConstants[i]);
+			int invocationInsnIndex = 1;
+			ASMAssertions.assertInvokesMethod(methodInvocation.instructions.get(invocationInsnIndex), Methods.class, constantConsumerName, 
+					constantConsumerDescriptor);
+			assertEquals(expectedLiteralValue, AbstractInsnNodes.getLiteralValue(methodInvocation.instructions.get(invocationInsnIndex - 1)));
+			uninliner.transformMethod(InstructionMocker.CLASS_NAME, methodInvocation);
+			int minimumInsnCount = 2 * (expectedConstantCombination.length - 1) + 1;
+			assertTrue(methodInvocation.instructions.size() >=  minimumInsnCount, 
+					String.format("Expected at least %d instructions, found %d", minimumInsnCount, methodInvocation.instructions.size()));
+			invocationInsnIndex += minimumInsnCount - 1;
+			ASMAssertions.assertInvokesMethod(methodInvocation.instructions.get(invocationInsnIndex), Methods.class, 
+					constantConsumerName, constantConsumerDescriptor);
+			ASMAssertions.assertReadsField(methodInvocation.instructions.get(0), Constants.class, 
+					expectedConstantCombination[0], constantTypeDescriptor);
+			for (int j = 1; j < expectedConstantCombination.length; j += 2)
+			{
+				ASMAssertions.assertReadsField(methodInvocation.instructions.get(j), Constants.class, 
+						expectedConstantCombination[j], constantTypeDescriptor);
+				ASMAssertions.assertOpcode(methodInvocation.instructions.get(j + 1), orOpcode);
+			}
+		}
+	}
 }
